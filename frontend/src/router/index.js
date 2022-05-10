59d201b20f2b2a533f7941fbfcf6c6950d9fda90
Marie <marie.durrmeyer@gmail.com>
2022-05-10 15:01:36 +0100

    commit

---
+++ frontend/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  
+ /* {
+   /* path: '/about',
+    name: 'about',
+    
+    component: () => import(/* webpackChunkName: "about" '../views/AboutView.vue')
+  }*/
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
