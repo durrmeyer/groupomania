1e2068eac3a3d547de11f7c58c68118e346e58e6
Mati35 <47659225+durrmeyer@users.noreply.github.com>
2023-01-11 06:39:37 +0100

    Add files via upload
    changement du nom du fichier

---
+++ frontend/src/components/comment/CommentAdd.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="add-blog">
+    <h2>Nouveau commentaire</h2>
+    <form @submit.prevent="addComment">
+      <label>Votre commentaire:</label>
+      <textarea
+        v-model="data.content"
+        label="content"
+        name="content"
+        required
+      ></textarea>
+      <button type="submit" class="button" @click.prevent="addComment()">
+        Poster
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommentAdd",
+
+  data() {
+    return {
+      token: localStorage.getItem("token"),
+      UserId: localStorage.getItem("UserId"),
+      data: { content: "", UserId: localStorage.getItem("UserId") },
+    };
+  },
+
+  methods: {
+    addComment() {
+      this.$store.dispatch("getPostById", this.$route.params.id);
+      this.$store.dispatch("createComment", {
+        id: this.$route.params.id,
+        data: this.data,
+      });
+      this.$router.push("/posts");
+    },
+  },
+};
+</script>
